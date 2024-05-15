24f3c9feb4e6c7f8d6fdf0c446ccd005219e39f2
GathangaElijah <elyg3672@gmail.com>
2024-05-15 15:43:18 +0300

    Change: Added a variable to the Ascii Processor function to hold the return value of the function

--- asciiart/ascii_processor.go
+++ asciiart/ascii_processor.go
@@ -2,11 +2,11 @@ package asciiart
 
 import (
 	"errors"
-	"fmt"
 	"strings"
 )
 
 func AsciiProcessor(txtInput string, asciiFile []byte) (string, error) {
+	var output = ""
 	if txtInput == "" {
 		return "", errors.New("empty string")
 	} else if txtInput == "\\n" {
@@ -17,16 +17,16 @@ func AsciiProcessor(txtInput string, asciiFile []byte) (string, error) {
 
 	for _, txt := range inputSubStr {
 		if txt == "\\n" || txt == "" {
-			fmt.Println()
+			output += "\n"
 			continue
 		}
 		for i := 0; i < 8; i++ {
 			for _, txtChar := range txt {
 				start := int(((txtChar - 32) * 9) + 1)
-				fmt.Print(asciiChars[start+i])
+				output += asciiChars[start+i]
 			}
-			fmt.Println()
+			output += "\n"
 		}
 	}
-	return "", nil
+	return output, nil
 }
