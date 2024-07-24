7786b628d968d79691dbc0b5e0775159219650e2
egathang <elyg3672@gmail.com>
2024-07-24 21:54:28 +0300

    looking for a string from another string

---
+++ wordmatch/wordmatch.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func WdMatch(str1, str2 string) string{
+	// var charMap = make(map[rune]bool)
+	var result = ""
+	// for _, char := range str2{
+	// 	charMap[char] = true
+	// }
+	// for _, char2 := range str1{
+	// 	if _, ok := charMap[char2]; ok{
+	// 		result += string(char2)
+	// 	} else {
+	// 		return ""
+	// 	}
+	// }
+	start := 0
+	
+		for i := 0; i < len(str1); i++{
+			for j:= start; j <len(str2); j++{
+				if str1[i] == str2[j]{
+					result += string(str1[i])
+					start = j
+					break
+				} 
+			}
+		}
+if result == str1{
+	return result
+}
+
+return ""
+}
+
+
+func main(){
+	string1 := "faya" 
+	string2 := "fgvvfdxcacpolhyghbreda"
+	fmt.Println(WdMatch(string1, string2))
+}
