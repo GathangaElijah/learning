6690fc79b385a5fd3ddcb0a890a98f6912f8291d
GathangaElijah <elyg3672@gmail.com>
2025-02-02 00:06:11 +0300

    chore: added a question for the excercise

--- Channels/exercise_1.go
+++ Channels/exercise_1.go
@@ -5,6 +5,13 @@ import (
 	"sync"
 )
 
+// Create a function that launches three goroutines that communicate using
+// a channel. The first two goroutines each write 10 numbers to the
+// channel. The third goroutine reads all the numbers from the channel and
+// prints them out. The function should exit when all values have been
+// printed out. Make sure that none of the goroutines leak. You can create
+// additional goroutines if needed.
+
 func Process() {
 	ch := make(chan int)
 
